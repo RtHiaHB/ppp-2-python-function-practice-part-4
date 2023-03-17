97385a03df3d79425220ea4f519e9b23fdd6785d
Thomas Davidson <tomd1969@gmail.com>
2023-03-17 14:28:10 -0400

    Added Pascal's triangle.

--- project.py
+++ project.py
@@ -33,3 +33,37 @@ def num_within(num_1, range_lo, range_hi):
 print(f"Is 3 between 2 and 4 (inclusive)?: {num_within(3, 2, 4)}")
 print(f"Is 3 between 1 and 3 (inclusive)?: {num_within(3, 1, 3)}")
 print(f"Is 10 between 2 and 5 (inclusive)?: {num_within(10, 2, 5)}")
+
+def pascal(n):
+    triangle = [[1], [1, 1]]
+    #base case
+    if n < 1:
+        print("invalid number of rows")
+    elif n == 1:
+        print(triangle[0])
+    else:
+        row_number = 2
+        #fill up correct number of rows in triangle
+        while len(triangle) < n:
+            row = []
+            row_prev = triangle[row_number - 1]
+            #create correct row, then add to triangle (this row will be 1 longer than row before it)
+            length = len(row_prev) + 1
+            for i in range(length):
+                #first number is 1
+                if i == 0:
+                    row.append(1)
+                elif i > 0 and i < length - 1:
+                    row.append(triangle[row_number -1][i - 1] + triangle[row_number-1][i])
+                #last number is 1
+                else:
+                    row.append(1)
+            triangle.append(row)
+            row_number += 1
+        for row in triangle:
+            print(row)
+
+print(f"row 2 in Pascal's triangle:")
+pascal(2)
+print(f"row 5 in Pascal's triangle:")
+pascal(5)
