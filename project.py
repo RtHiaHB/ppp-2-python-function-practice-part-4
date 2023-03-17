e35e7a11e84e6dfb2744b1e20ef1ff6fbf6f705e
Thomas Davidson <tomd1969@gmail.com>
2023-03-17 14:11:24 -0400

    Added mult_list and rev_string

--- project.py
+++ project.py
@@ -9,4 +9,20 @@ def max_num(a, b, c):
             greatest = i
     return greatest
 
-print(max_num(3, 10, 8))
+print(f"Max of 3, 10, and 8: {max_num(3, 10, 8)}")
+
+def mult_list(*args):
+    product = 1
+    for i in args:
+        product = product * i
+    return product
+
+print(f"Product of 3, 8, and 10: {mult_list(3, 8, 10)}")
+
+def rev_string(my_str):
+    reversed = ""
+    for i in range(len(my_str) - 1, -1, -1):
+        reversed =  reversed + my_str[i]
+    return reversed
+
+print(f"'Thomas' reversed: {rev_string('Thomas')}")
